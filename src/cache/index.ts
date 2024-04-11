601ae73ad30235e7a6204314c1db314fdae7f6c2
aminwhat <m.aminwhat@gmail.com>
2024-04-11 10:55:59 +0330

    collectionInit

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,4 +1,10 @@
-import { createRxDatabase, RxDatabase } from "rxdb";
+import {
+  createRxDatabase,
+  JsonSchemaTypes,
+  RxCollectionCreator,
+  RxDatabase,
+  TopLevelProperty,
+} from "rxdb";
 import { getRxStorageDexie } from "rxdb/plugins/storage-dexie";
 
 export class PoppyCache {
@@ -18,7 +24,97 @@ export class PoppyCache {
     });
   }
 
+  /**
+   * Give it a json sample of the database data and it will build it in the client
+   */
+  async collectionInit(colls: { [name: string]: any }) {
+    const collections: {
+      [x: string]: RxCollectionCreator<any>;
+    } = {};
+
+    Object.keys(colls).map((name) => {
+      collections[name] = convertObjToCollection(colls[name]);
+    });
+
+    await this.cache.addCollections(collections);
+  }
+
   async destroy() {
     await this.cache.remove();
   }
+}
+
+function convertObjToType(obj: any): JsonSchemaTypes {
+  let type: JsonSchemaTypes = "null";
+
+  switch (typeof obj) {
+    case "bigint":
+      type = "integer";
+      break;
+    case "boolean":
+      type = "boolean";
+      break;
+    case "number":
+      type = "number";
+      break;
+    case "string":
+      type = "string";
+      break;
+    default:
+      type = "array";
+      break;
+  }
+
+  return type;
+}
+
+function convertObjToCollectionPart(obj: any): {
+  [x: string]: TopLevelProperty;
+} {
+  const result: { [x: string]: TopLevelProperty } = {};
+
+  Object.keys(obj).map((val) => {
+    if (typeof obj[val] === "object") {
+      result[val] = convertObjToCollectionPart(obj[val]);
+    } else {
+      result[val] = { type: convertObjToType(obj[val]) };
+    }
+  });
+
+  return result;
+}
+
+function convertObjToCollection(obj: any): RxCollectionCreator {
+  let primaryKey = "id";
+  let version = 0;
+  let properties: { [x: string]: TopLevelProperty } = {};
+
+  if (obj._id) {
+    primaryKey = "_id";
+  }
+
+  if (obj.__v) {
+    try {
+      version = Number(obj.__v);
+    } catch (error) {
+      console.log(`Error Converting __v: ${error}`);
+    }
+  }
+
+  Object.keys(obj).map((val) => {
+    if (typeof obj[val] === "object") {
+      properties[val] = convertObjToCollectionPart(obj[val]);
+    } else {
+      properties[val] = { type: convertObjToType(obj[val]) };
+    }
+  });
+
+  return {
+    schema: {
+      primaryKey,
+      version: version,
+      type: "object",
+      properties,
+    },
+  };
 }
