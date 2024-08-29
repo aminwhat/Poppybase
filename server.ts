73aaa4a7f98c80acf70a14b37e1cb063dc4822aa
aminwhat <m.aminwhat@gmail.com>
2024-08-29 22:11:43 +0330

    use apiRouter

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
-import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 import express from "express";
+import apiRouter from "./api/router";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -10,6 +10,8 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+
+  server.use("/api", apiRouter);
 
   server.all("*", (req, res) => {
     const parsedUrl = parse(req.url!, true);
